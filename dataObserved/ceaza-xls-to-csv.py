11a8f83bc0bde02dfb399438c1df80f956c5ab34
Oscar Carrasco <osc.carrasco@gmail.com>
2018-04-18 21:20:21 +0200

    Fix arg in ceaza...py

--- dataObserved/ceaza-xls-to-csv.py
+++ dataObserved/ceaza-xls-to-csv.py
@@ -1,11 +1,12 @@
 #!/usr/bin/env python
 
 import os
-import sys
+import argparse
 import glob
 import pandas as pd
 
-def data(path, name_csv):
+
+def data(path, name_csv=False):
     os.chdir(path)
     files_xlsx = sorted(glob.glob("*.xls"))
     df = pd.DataFrame()
@@ -15,12 +16,22 @@ def data(path, name_csv):
                              parse_dates=[0], usecols=[0, 2, 6],
                              names=['date', 'wind_speed', 'wind_dir'])
         data = data.dropna()
-        data['name'], waste = f.split(".")
-        df = df.append(data)
+        name, waste = f.split(".")
+        data['name'] = name
+        if name_csv is None:
+            data.to_csv(name + '.csv', index=False)
+        else:
+            df = df.append(data)
 
-    df.to_csv(name_csv + '.csv', index=False)
+    if name_csv is not None:
+        df.to_csv(name_csv + '.csv', index=False)
+
 
 if __name__ == "__main__":
-    files_path = sys.argv[1]
-    name_csv = sys.argv[2]
-    data(files_path, name_csv)
+    parser = argparse.ArgumentParser(prog='xls-to-csv', description='xls(s) to csv(s)')
+    parser.add_argument('-d', default=os.getcwd(), help='dir with xls(s)')
+    parser.add_argument('-n', action="store", help='if used, one csv will be generated. Otherwise for each xls one csv will be generated')
+    parser.add_argument
+
+    args = parser.parse_args()
+    data(args.d, args.n)
